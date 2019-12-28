35bcbaef41234cbb52cf4b702fbe7b5ace6d1988
gus <gustavo.graz@outlook.com>
2019-12-28 18:44:47 +0100

    Add main script and ontology package with onto_creation

---
+++ bad_smells.py
@@ -0,0 +1,50 @@
+import sys
+import argparse
+from utils.doc_utils import *
+from utils.misc import listget
+
+
+# add one gateway function for each functionality
+
+def create_ontology_gateway(args):
+    from ontology import onto_creator
+    onto_creator.create_ontology_argparse(args)
+
+
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers()
+
+# add subparser for find_god_classes
+p_create_ontology = subparsers.add_parser('onto_creator')
+p_create_ontology.add_argument('-s', '--source', dest='source', default='')
+p_create_ontology.set_defaults(func=create_ontology_gateway)
+
+
+def main(argv):
+    helpstrings = {'-h', '--help'}
+
+    command = listget(argv, 0, '').lower()
+
+    # The user did not enter a command, or the entered command is not recognized.
+    if command not in MODULE_DOCSTRINGS:
+        print(DOCSTRING)
+        if command == '':
+            print('You are seeing the default help text because you did not choose a command.')
+        elif command not in helpstrings:
+            print('You are seeing the default help text because "%s" was not recognized' % command)
+        return 1
+
+    # The user entered a command, but no further arguments, or just help.
+    argument = listget(argv, 1, '').lower()
+    if argument in helpstrings:
+        print(MODULE_DOCSTRINGS[command])
+        return 1
+
+    args = parser.parse_args(argv)
+    args.func(args)
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main(sys.argv[1:]))
---
+++ ontology/onto_creator.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import javalang
+import owlready2
+
+
+def create_ontology():
+    print('\n> Creating new ontology...')
+
+
+def create_ontology_argparse(args):
+    # if args.source is None or not os.path.exists(args.source):
+    #     print('Enter a valid path to a source code...')
+    #     sys.exit(0)
+
+    create_ontology()
