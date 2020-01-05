5668406ddb81b153538504c1423c21f6531bd018
gus <gustavo.graz@outlook.com>
2020-01-05 21:22:21 +0100

    Add visitor's implementation

--- ontology/onto_creator.py
+++ ontology/onto_creator.py
@@ -1,20 +1,30 @@
+import ast
 import os
 import sys
-import javalang
-import owlready2
+from owlready2 import get_ontology
 
-from definitions import ROOT_DIR
+from ontology.visitor import Visitor
+from definitions import RES_PATH
+
+ONTO_PATH = os.path.join(RES_PATH, 'tree.owl')
 
 
-def create_ontology():
+def create_ontology(file_path):
     print('\n> Creating new ontology...')
-    
+
+    with open(file_path, "r") as source:
+        tree = ast.parse(source.read())
+
+    onto = get_ontology("http://my.onto.org/tree.owl")
+
+    visitor = Visitor(onto)
+    visitor.visit(tree)
+    onto.save(ONTO_PATH, format="rdfxml")
 
 
 def create_ontology_argparse(args):
-
     if args.source is None or not (os.path.exists(args.source) and os.path.isfile(args.source)):
         print('Enter a valid path to a a file "tree.py"...')
         sys.exit(0)
 
-    create_ontology()
+    create_ontology(file_path=args.source)
--- ontology/visitor.py
+++ ontology/visitor.py
@@ -1,6 +1,28 @@
 import ast
+from owlready2 import *
 
 
 class Visitor(ast.NodeVisitor):
+
+    def __init__(self, onto):
+        self.onto = onto
+
     def generic_visit(self, node):
         ast.NodeVisitor.generic_visit(self, node)
+        with self.onto as onto:
+            if type(node) == ast.ClassDef:
+                for a in node.bases:
+                    if a.id == "Node":
+                        types.new_class(node.name, (Thing,))
+                    else:
+                        types.new_class(node.name, (onto[a.id],))
+
+            elif type(node) == ast.Assign:
+                for b in node.value.elts:
+                    if b.s != "body" and b.s != "parameters" and b.s != "name":
+                        types.new_class(b.s, (DataProperty,))
+                    if b.s == "name":
+                        types.new_class("jname", (DataProperty,))
+                    if b.s == "body" or b.s == "parameters":
+                        types.new_class(b.s, (ObjectProperty,))
+
